1b9bc2737082021ff61ee3834ab1710290f6dbf2
toufiq2018331084 <fahim.prime@gmail.com>
2020-06-05 01:34:39 +0600

    doctor search(in progress)

--- app/src/main/java/com/hasib/carebear/patient/DoctorSearch.java
+++ app/src/main/java/com/hasib/carebear/patient/DoctorSearch.java
@@ -1,4 +1,14 @@
 package com.hasib.carebear.patient;
 
-public class DoctorSearch {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DoctorSearch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_search);
+    }
 }
--- app/src/main/java/com/hasib/carebear/patient/DoctorSearchAdapter.java
+++ app/src/main/java/com/hasib/carebear/patient/DoctorSearchAdapter.java
@@ -1,5 +1,7 @@
 package com.hasib.carebear.patient;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
@@ -9,39 +11,57 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.hasib.carebear.R;
+import com.hasib.carebear.doctor.container.UserDetails;
+
+import java.util.List;
+import java.util.concurrent.RecursiveAction;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class DoctorSearchAdapter {
-    public class viewHolder extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class DoctorSearchAdapter extends RecyclerView.Adapter<DoctorSearchAdapter.viewHolder>{
 
-        CircleImageView profilePic;
+    private Context context;
+    private List<UserDetails> doctorList;
+
+    public DoctorSearchAdapter(Context context, List<UserDetails> doctorList) {
+        this.context = context;
+        this.doctorList = doctorList;
+    }
+
+    @NonNull
+    @Override
+    public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.doctor_list_layout,parent,false);
+        return new viewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewHolder holder, int position) {
+        UserDetails userDetails = doctorList.get(position);
+        holder.name.setText(userDetails.getFullName());
+
+        // TODO: 6/3/20 bind other details of doctor to the UI elements
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return doctorList.size();
+    }
+
+    class viewHolder extends RecyclerView.ViewHolder{
+
         TextView name;
-        RelativeLayout doctorListLayout;
+        // TODO: 6/3/20 declare other thing to show while searching doctor
 
 
         public viewHolder(@NonNull View itemView) {
             super(itemView);
-            profilePic = itemView.findViewById(R.id.idDoctorProfilePic);
-            name = itemView.findViewById(R.id.iddoctorName);
-            doctorListLayout = itemView.findViewById(R.id.id_doctor_list_layout);
 
-        }
+            name = itemView.findViewById((R.id.iddoctorName);
+            // TODO: 6/3/20  find the things declared in the todo above this
 
-        @NonNull
-        @Override
-        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            return null;
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
-        }
-
-        @Override
-        public int getItemCount() {
-            return 0;
         }
     }
 }
